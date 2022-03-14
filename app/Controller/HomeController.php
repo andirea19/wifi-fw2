801cfdb9fcf2c546d521b35ef5ba785b4cf566e7
Thomas Macher <machernator@gmail.com>
2022-03-14 12:20:49 +0100

    dynamische contents

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -4,6 +4,12 @@ namespace Controller;
 class HomeController {
 	public function index($f3, $params)
 	{
+		// Jede Controller Methode, die von einer route aufgerufen wird,
+		// muss die Variable content deklarieren. Sie zeigth auf eine
+		// Datei in contentPath.
+		$f3->set('content', $f3->get('contentPath') . 'home.html');
+		// JEDE Controller Methode, die von einer route aufgerufen wird,
+		// muss nur noch index.html im views folder anzeigen.
 		echo \Template::instance()->render($f3->get('viewsPath') . 'index.html');
 	}
 }
--- config.php
+++ config.php
@@ -5,9 +5,10 @@
 // Pfad zum views folder
 $f3->set('viewsPath', '/views/');
 // Pfad zu den einzelnen Contents der Seiten
-$f3->set('contentPath', $f3->get('viewsPath') . 'content');
+$f3->set('contentPath', $f3->get('viewsPath') . 'content/');
 // Resources Pfad
 $f3->set('resourcesPath', '/resources/');
+//
 $f3->set('cssPath', $f3->get('resourcesPath') . 'css/');
 $f3->set('jsPath', $f3->get('resourcesPath') . 'js/');
 $f3->set('imagePath', $f3->get('resourcesPath') . 'img/');

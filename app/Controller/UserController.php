593566c12a84e17ecdd9bb4dbe789fc637129a15
Thomas Macher <machernator@gmail.com>
2022-03-15 09:55:25 +0100

    UserController basics

---
+++ app/Controller/UserController.php
@@ -0,0 +1,9 @@
+<?php
+namespace Controller;
+
+class UserController {
+	public function index($f3, $params)
+	{
+		echo 'User Controller';
+	}
+}
--- routes.php
+++ routes.php
@@ -3,3 +3,6 @@
 // erhalten wir eine GET Anfrage auf die Homepage, rufe im namespace
 // Controller die Methode index der Klasse HomeController auf.
 $f3->route('GET /', '\Controller\HomeController->index');
+
+// User
+$f3->route('GET /user', '\Controller\UserController->index');

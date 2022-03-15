fbc1892dd5037328a4f89c7960cf8e2c0394e13f
Thomas Macher <machernator@gmail.com>
2022-03-15 09:36:46 +0100

    Username Home hinzugefügt

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -8,6 +8,7 @@ class HomeController {
 		// muss die Variable content deklarieren. Sie zeigth auf eine
 		// Datei in contentPath.
 		$f3->set('content', $f3->get('contentPath') . 'home.html');
+		$f3->set('username', 'Thomas Macher');
 		// JEDE Controller Methode, die von einer route aufgerufen wird,
 		// muss nur noch index.html im views folder anzeigen.
 		echo \Template::instance()->render($f3->get('viewsPath') . 'index.html');

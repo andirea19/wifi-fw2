10228e9584a403ab4c20bcc41b029b939097569e
Thomas Macher <machernator@gmail.com>
2022-03-17 12:29:10 +0100

    Anleitung delete

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -20,4 +20,12 @@ class UserController {
 	{
 		echo 'User Id: ' . $params['id'];
 	}
+
+	public function delete($f3, $params) {
+		// Id aus params auslesen
+
+		// Id an UserModel->delete übergeben
+
+		// nach erfolgreichem Löschen auf user redirect (oder index Methode aufrufen)
+	}
 }
